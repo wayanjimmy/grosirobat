3a99ec4561bc4aa2f995555f58cd735349ec2172
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-04-27 08:45:17 +0800

    header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
+
+class Header extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen,
+    }))
+  }
+
+  render() {
+    return (
+      <Navbar light expand="md">
+        <NavbarBrand href="/">Paramore</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Products
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Manage Products</DropdownItem>
+                <DropdownItem>Manage Units</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Admin
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Manage Users</DropdownItem>
+                <DropdownItem>Change Password</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Logout</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    )
+  }
+}
+
+export default Header
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
