afc757833a5a1debb71cb9c94cf0c5bdb4d43d9c
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-04-27 09:06:51 +0800

    update styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
 import Header from './Header'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="wrapper">
         <Header />
+        <Container className="content">
+          <h3>Content</h3>
+        </Container>
+        <div className="footer">
+          <Container>
+            <Row>
+              <Col className="text-center">
+                <small>paramore</small>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     )
   }
