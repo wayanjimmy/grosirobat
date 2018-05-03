e7e61855e6517b36da387b5497ca691e01a1d1ac
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-05-03 20:33:20 +0800

    using json serializer

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -14,9 +14,11 @@ class ApiController < ActionController::API
 
   def pagination_dict(collection, additional = {})
     pagination_meta = {
-      total_pages: collection.total_pages,
+      current_page: collection.current_page,
       prev_page: collection.previous_page,
-      next_page: collection.next_page
+      next_page: collection.next_page,
+      total_pages: collection.total_pages,
+      total_count: collection.total_entries
     }
     pagination_meta.merge(additional)
   end
